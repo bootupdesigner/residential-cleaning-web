c9fb4cca0ed4157c02e89335caea32340d414879
bootupdesigner <bootup.itinfo@gmail.com>
2025-06-05 16:17:49 -0400

    updated  footer to contain 4 menus

--- components/Footer.js
+++ components/Footer.js
@@ -20,24 +20,95 @@ const Footer = () => {
   }, [isAuthenticated]);
 
   return (
-    <footer style={styles.container}>
-      {isLoggedIn ? (
-        <div style={styles.container}>
-          <Link href="/privacy-policy">Privacy Policy</Link>
-          <Link href="/user-profile">Profile</Link>
-          <span onClick={handleLogout} style={styles.logoutText}>Logout</span>
+    <footer>
+      <div className='bg-dark' style={{ color: 'white', padding: '20px', }}>
+
+        <div className='row'>
+
+          <div className='list-group list-group-flush col-3 col-md-3 col-sm-12'>
+
+            <h4 style={{ textDecoration: 'underline' }}>Quick Links</h4>
+
+
+            {isLoggedIn ?
+              (
+                <>
+                  <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='/booking'>Schedule an Appointment</Link>
+
+                  <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='/appointments'>Upcoming Appointments</Link>
+
+                  <span 
+                  onClick={handleLogout} 
+                  className='list-group-item bg-dark text-white logoutText' >Logout</span>
+
+                  <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='/user-profile'>Profile</Link>
+                </>
+              ) : (
+                <>
+                  <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='/signup'>Sign Up</Link>
+
+                  <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='/signin'>Sign In</Link>
+
+                </>
+              )
+            }
+          </div>
+
+          <div className='list-group list-group-flush col-3 col-md-3 col-sm-12'>
+
+            <h4 style={{ textDecoration: 'underline', }}>Cleaning Services</h4>
+
+            <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='#'>Move-Out Cleaning</Link>
+
+            <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='#'>Office Cleaning</Link>
+
+            <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='#'>Commercial Cleaning Services</Link>
+
+          </div>
+
+          <div className='list-group list-group-flush col-3 col-md-3 col-sm-12'>
+
+            <h4 style={{ textDecoration: 'underline', }}>About JMAC</h4>
+
+            <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='/privacy-policy'>Privacy Policy</Link>
+
+            <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='/terms-of-service'>Terms of Service</Link>
+
+          </div>
+
+          <div className='list-group list-group-flush col-3 col-md-3 col-sm-12'>
+
+            <h4 style={{ textDecoration: 'underline', }}>Contact JMAC</h4>
+
+            <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='tel:+13057937344'>(305)793-7344</Link>
+
+            <Link className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }} href='mailto:shepardcurtis2@gmail.com'>shepardcurtis2@gmail.com</Link>
+
+            <p className='list-group-item bg-dark text-white' style={{ textDecoration: 'none', }}>Monday - Friday  8:00am -7:00pm</p>
+
+          </div>
         </div>
-      ) : (
-        <div style={styles.container}>
-          <Link href="/privacy-policy">Privacy Policy</Link>
-          <Link href="/signin">Sign In</Link>
-        </div>
-      )}
+
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+// <footer style={styles.container}>
+//   {isLoggedIn ? (
+//     <div style={styles.container}>
+//       <Link href="/privacy-policy">Privacy Policy</Link>
+//       <Link href="/user-profile">Profile</Link>
+//       <span onClick={handleLogout} style={styles.logoutText}>Logout</span>
+//     </div>
+//   ) : (
+//     <div style={styles.container}>
+//       <Link href="/privacy-policy">Privacy Policy</Link>
+//       <Link href="/signin">Sign In</Link>
+//     </div>
+//   )}
+// </footer>
 
 const styles = {
   container: {
--- components/PriceGrid.js
+++ components/PriceGrid.js
@@ -4,85 +4,85 @@ import React from 'react'
 
 function PriceGrid() {
     return (
-        <div class="container text-center" style={{marginTop:'20px',}}>
+        <div className="container text-center" style={{marginTop:'20px',}}>
 
             <h3>Home Cleaning Prices</h3>
 
             <p>Sign up for an Instant Quote!</p>
 
-            <div class=" row row-cols-6 g-4">
+            <div className="row row-cols-6 g-4">
 
-                <div class="col"></div>
+                <div className="col"></div>
 
-                <div class="col border border-danger">1 Bd</div>
+                <div className="col border border-danger">1 Bd</div>
 
-                <div class="col border border-success">2 Bd</div>
+                <div className="col border border-success">2 Bd</div>
 
-                <div class="col border border-primary">3 Bd</div>
+                <div className="col border border-primary">3 Bd</div>
 
-                <div class="col border border-warning">4 Bd</div>
+                <div className="col border border-warning">4 Bd</div>
 
-                <div class="col border border-secondary">5 Bd</div>
+                <div className="col border border-secondary">5 Bd</div>
 
-                <div class="col border border-primary-subtle">1 Br</div>
+                <div className="col border border-primary-subtle">1 Br</div>
 
-                <div class="col border border-danger">$150</div>
+                <div className="col border border-danger">$150</div>
 
-                <div class="col border border-success">$185</div>
+                <div className="col border border-success">$185</div>
 
-                <div class="col border border-primary">$220</div>
+                <div className="col border border-primary">$220</div>
 
-                <div class="col border border-warning">$255</div>
+                <div className="col border border-warning">$255</div>
 
-                <div class="col border border-secondary">$290</div>
+                <div className="col border border-secondary">$290</div>
 
-                <div class="col border border-primary-subtle">2 Br</div>
+                <div className="col border border-primary-subtle">2 Br</div>
 
-                <div class="col border border-danger">$175</div>
+                <div className="col border border-danger">$175</div>
 
-                <div class="col border border-success">$210</div>
+                <div className="col border border-success">$210</div>
 
-                <div class="col border border-primary">$245</div>
+                <div className="col border border-primary">$245</div>
 
-                <div class="col border border-warning">$280</div>
+                <div className="col border border-warning">$280</div>
 
-                <div class="col border border-secondary">$315</div>
+                <div className="col border border-secondary">$315</div>
 
-                <div class="col border border-primary-subtle">3 Br</div>
+                <div className="col border border-primary-subtle">3 Br</div>
 
-                <div class="col border border-danger">$200</div>
+                <div className="col border border-danger">$200</div>
 
-                <div class="col border border-success">$235</div>
+                <div className="col border border-success">$235</div>
 
-                <div class="col border border-primary">$270</div>
+                <div className="col border border-primary">$270</div>
 
-                <div class="col border border-warning">$305</div>
+                <div className="col border border-warning">$305</div>
 
-                <div class="col border border-secondary">$340</div>
+                <div className="col border border-secondary">$340</div>
 
-                <div class="col border border-primary-subtle">4 Br</div>
+                <div className="col border border-primary-subtle">4 Br</div>
 
-                <div class="col border border-danger">$225</div>
+                <div className="col border border-danger">$225</div>
 
-                <div class="col border border-success">$260</div>
+                <div className="col border border-success">$260</div>
 
-                <div class="col border border-primary">$295</div>
+                <div className="col border border-primary">$295</div>
 
-                <div class="col border border-warning">$330</div>
+                <div className="col border border-warning">$330</div>
 
-                <div class="col border border-secondary">$365</div>
+                <div className="col border border-secondary">$365</div>
 
-                <div class="col border border-primary-subtle">5 Br</div>
+                <div className="col border border-primary-subtle">5 Br</div>
 
-                <div class="col border border-danger">$250</div>
+                <div className="col border border-danger">$250</div>
 
-                <div class="col border border-success">$285</div>
+                <div className="col border border-success">$285</div>
 
-                <div class="col border border-primary">$320</div>
+                <div className="col border border-primary">$320</div>
 
-                <div class="col border border-warning">$355</div>
+                <div className="col border border-warning">$355</div>
 
-                <div class="col border border-secondary">$390</div>
+                <div className="col border border-secondary">$390</div>
 
             </div>
 
